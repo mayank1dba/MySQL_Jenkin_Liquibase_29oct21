29f646945bed4c4d387e652276be0f130627e0f3
mayank1dba <78435783+mayank1dba@users.noreply.github.com>
2021-10-29 17:09:06 +0800

    Update changelog.mysql.sql

--- changelog.mysql.sql
+++ changelog.mysql.sql
@@ -1,10 +1,10 @@
 --liquibase formatted sql
 /* https://www.liquibase.org/documentation/sql_format.html */
 
---changeset May:1
+--changeset May:41
 --drop table person2;
 
---changeset May_1:2
+--changeset May_1:11
 create table person2 (
     id int primary key,
     name varchar(50) not null,
@@ -13,10 +13,10 @@ create table person2 (
     city varchar(30)
 )
 
---changeset Raji:3
+--changeset Raji:21
 --drop table company2
 
---changeset Raji_1:4
+--changeset Raji_1:31
 create table company2 (
     id int primary key,
     name varchar(50) not null,
